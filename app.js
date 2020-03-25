6a06be13875c1e8e7d8f6f117ced8d897ac81e13
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-03-25 16:30:51 -0700

    step 2

---
+++ app.js
@@ -0,0 +1,20 @@
+// Step 2: Creat an Array of Objects and Log to the Console. 
+    // 5 objects 
+    // 2-3 properties of the object 
+    // One property needs to be something that I can test and sort into 2 or more categories.  
+    //Put all 5 objects into an array. Store this array in a const
+    //Loop through this array and log out each object to the console. Serve your project in live server and open up the javascript console to check that it works 
+    //ACP 
+
+//Different Cats 
+
+const vincent = { name: 'vincent', age: 6, weight: 16 };
+const bagheera = { name: 'bagheera', age: 3, weight: 12 };
+const kuxa = { name: 'kuxa', age: 4, weight: 11 };
+const marley = { name: 'marley', age: 9, weight: 11 };
+const jill = { name: 'vincent', age: 7, weight: 15 };
+
+const cats = [vincent, bagheera, kuxa, marley, jill];
+
+
+    
